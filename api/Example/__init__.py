0870d6535e91dbcccef40708bf85191017dee0ab
Jim Kytola <jkytola@nanthealth.com>
2021-03-29 08:23:42 -0400

    Move import back to top / testing azure updates

--- api/Example/__init__.py
+++ api/Example/__init__.py
@@ -1,8 +1,8 @@
 import logging
 import azure.functions as func
+import pyodbc
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Made it")
-    import pyodbc
     return func.HttpResponse("Made it!")
